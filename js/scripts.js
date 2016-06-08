bbaeaf1067fef0fd18627db42ea5527a17b09544
Adam Luchini and Kyle Helyar <adam@>
2016-06-08 14:47:21 -0700

    add js that will only add ay to the end of the word if its a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,21 @@ $(function () {
   $('form#pl').submit(function(event) {
     event.preventDefault();
     var word = $('#inputWord').val();
+    var pigLatin = ('');
+
+    var words = [$('#inputWord').val()];
+    var vowels = ['a', 'e', 'i' , 'o', 'u', 'y'];
+
+    for (i=0, x=0; i <= vowels.length; i++) {
+      if ($(vowels).toString([i]) === $(words).toString([x])) {
+        pigLatin = word + 'ay';
+      }
+    }
 
 
 
 
-    pigLatin = word + 'ay';
+
 
 
 
